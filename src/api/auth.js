faaaee564602a34ec78f6b18248abc4e9f3f09ba
jeff0518 <cycivs07@icloud.com>
2022-12-05 20:00:10 +0800

    feat:

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,39 +1,35 @@
 import axios from 'axios';
 
 const authURL = 'https://todo-list.alphacamp.io/api/auth';
+
 export const login = async ({ username, password }) => {
   try {
     const { data } = await axios.post(`${authURL}/login`, {
       username,
       password,
     });
-
     const { authToken } = data;
-
-    if (!!authToken) {
+    if (authToken) {
       return { success: true, ...data };
     }
-
     return data;
   } catch (error) {
-    console.error('[Login Failed]:', error);
+    console.error('[Login Failed]: ', error);
   }
 };
 
-export const register = async ({ username, password, email }) => {
+export const register = async ({ username, email, password }) => {
   try {
     const { data } = await axios.post(`${authURL}/register`, {
       username,
-      password,
       email,
+      password,
     });
-
     const { authToken } = data;
-
-    if (!!authToken) {
+    console.log(data);
+    if (authToken) {
       return { success: true, ...data };
     }
-
     return data;
   } catch (error) {
     console.error('[Register Failed]: ', error);
@@ -49,6 +45,6 @@ export const checkPermission = async (authToken) => {
     });
     return response.data.success;
   } catch (error) {
-    console.error('[Check Permission Failed]:', error);
+    console.error('[Check Permission Failed]: ', error);
   }
 };
